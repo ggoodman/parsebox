a2aa7b501f3340f6161884ce4180b00e93fcf12c
sinclair <haydn.developer@gmail.com>
2024-11-10 14:18:49 +0900

    bnf

--- example/index.ts
+++ example/index.ts
@@ -1,71 +1,107 @@
-import { Static } from '@sinclair/parsebox'
-import { Parse } from './typebox'
+import { Runtime } from '@sinclair/parsebox'
 
 // ------------------------------------------------------------------
-// Example: TypeBox
+// Const
 // ------------------------------------------------------------------
+const Const = Runtime.String(['\'', '"'], value => Runtime.Const(value))
 
-const T = Parse(`{
-  x: number,
-  y: number,
-  z: number  
+// ------------------------------------------------------------------
+// Reference
+// ------------------------------------------------------------------
+const Reference = Runtime.Ident(value => Runtime.Ref(value))
+
+// ------------------------------------------------------------------
+// Elements
+// ------------------------------------------------------------------
+const Base = Runtime.Union([
+  Runtime.Ref('Const'),
+  Runtime.Ref('Reference')
+])
+
+// ------------------------------------------------------------------
+// Elements
+// ------------------------------------------------------------------
+const Elements = Runtime.Union([
+  Runtime.Tuple([Runtime.Ref('Base'), Runtime.Const(','), Runtime.Ref<Runtime.IParser[]>('Elements')]),
+  Runtime.Tuple([Runtime.Ref('Base')]),
+  Runtime.Tuple([])
+], value => {
+  return (
+    value.length === 3 ? [value[0], ...value[2]] :
+    value.length === 1 ? [value[0]] :
+    []
+  )
+})
+
+// ------------------------------------------------------------------
+// Tuple
+// ------------------------------------------------------------------
+const Tuple = Runtime.Union([
+  Runtime.Tuple([Runtime.Const('['), Runtime.Ref<Runtime.IParser[]>('Elements'), Runtime.Const(']')]),
+  Runtime.Tuple([Runtime.Const('['), Runtime.Const(']')]),
+], value => {
+  return value.length === 3 
+    ? Runtime.Tuple(value[1])
+    : Runtime.Tuple([])
+})
+
+// ------------------------------------------------------------------
+// Variants
+// ------------------------------------------------------------------
+const Variant = Runtime.Union([
+  Runtime.Ref('Tuple'),
+  Runtime.Ref('Base')
+])
+
+const Variants = Runtime.Union([
+  Runtime.Tuple([Runtime.Ref('Variant'), Runtime.Const('|'), Runtime.Ref<Runtime.IParser[]>('Variants')]),
+  Runtime.Tuple([Runtime.Ref('Variant')]),
+  Runtime.Tuple([])
+], value => {
+  return (
+    value.length === 3 ? [value[0], ...value[2]] :
+    value.length === 1 ? [value[0]] :
+    []
+  )
+})
+
+// ------------------------------------------------------------------
+// Union
+// ------------------------------------------------------------------
+const Union = Runtime.Union([
+  Runtime.Ref<Runtime.IParser[]>('Variants')
+], value => Runtime.Union(value))
+
+// ------------------------------------------------------------------
+// Match
+// ------------------------------------------------------------------
+const Match = Runtime.Union([
+  Runtime.Ref('Union'),
+  Runtime.Ref('Tuple')
+])
+
+const Syntax = Runtime.Tuple([
+  Runtime.Const('syntax'),
+  Runtime.Ident(),
+  Runtime.Const('='),
+  Runtime.Ref('Match')
+])
+
+const Module = new Runtime.Module({
+  Base,
+  Reference,
+  Const,
+  Elements,
+  Tuple,
+  Variant,
+  Variants,
+  Union,
+  Match,
+  Syntax,
+})
+
+const R = Module.Parse('Syntax', `syntax A = ['A', A | B] | [] = (value) => {
+  
 }`)
 
-console.log(T)
-
-// ------------------------------------------------------------------
-// Example: Expression Parser
-// ------------------------------------------------------------------
-
-type Result = Static.Parse<Expr, 'x * (y + z)'> // hover
-
-// prettier-ignore
-type BinaryReduce<Left extends unknown, Right extends unknown[]> = (
-  Right extends [infer Operator, infer Right, infer Rest extends unknown[]] 
-    ? { left: Left; operator: Operator; right: BinaryReduce<Right, Rest> } 
-    : Left
-)
-
-// prettier-ignore
-interface BinaryMapping extends Static.IMapping {
-  output: this['input'] extends [infer Left, infer Right extends unknown[]] 
-    ? BinaryReduce<Left, Right> 
-    : never
-}
-
-// prettier-ignore
-interface FactorMapping extends Static.IMapping {
-  output: (
-    this['input'] extends ['(', infer Expr, ')'] ? Expr : 
-    this['input'] extends [infer Operand] ? Operand : 
-    never
-  )
-}
-
-type Operand = Static.Ident
-
-// prettier-ignore
-type Factor = Static.Union<[
-  Static.Tuple<[Static.Const<'('>, Expr, Static.Const<')'>]>, 
-  Static.Tuple<[Operand]>
-], FactorMapping>
-
-// prettier-ignore
-type TermTail = Static.Union<[
-  Static.Tuple<[Static.Const<'*'>, Factor, TermTail]>, 
-  Static.Tuple<[Static.Const<'/'>, Factor, TermTail]>, 
-  Static.Tuple<[]>
-]>
-
-// prettier-ignore
-type ExprTail = Static.Union<[
-  Static.Tuple<[Static.Const<'+'>, Term, ExprTail]>, 
-  Static.Tuple<[Static.Const<'-'>, Term, ExprTail]>, 
-  Static.Tuple<[]>
-]>
-
-// prettier-ignore
-type Term = Static.Tuple<[Factor, TermTail], BinaryMapping>
-
-// prettier-ignore
-type Expr = Static.Tuple<[Term, ExprTail], BinaryMapping>
+console.dir(JSON.parse(JSON.stringify(R)), { depth: 100 })
